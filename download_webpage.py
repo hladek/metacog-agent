8df0b9fd221047a9b726314c5d2f690c260e4d7a
Daniel Hladek <daniel.hladek@tuke.sk>
2026-01-07 18:39:24 +0100

    zz

--- download_webpage.py
+++ download_webpage.py
@@ -1,4 +1,5 @@
 import trafilatura
+import sys
 
 def download_webpage(url: str):
     """
@@ -9,13 +10,19 @@ def download_webpage(url: str):
     
     """
     downloaded = trafilatura.fetch_url(url)
+    #print(downloaded)
     
     metadata = trafilatura.extract_metadata(downloaded)
+    print(metadata.as_dict())
+
+    #comments = trafilatura.extract_comments(downloaded)
+    #print(comments)
+
     
     json_output = trafilatura.extract(
         downloaded,
-        include_comments=include_comments,
-        include_tables=include_tables,
+        include_comments=True,
+        include_tables=True,
         output_format='json'
     )
 
@@ -25,11 +32,17 @@ def download_webpage(url: str):
 
 if __name__ == '__main__':
     # Example usage
-    url = 'https://example.com'
+    url = sys.argv[1]
+    import trafilatura.sitemaps
+    import trafilatura.feeds
+    sitemap =  trafilatura.sitemaps.sitemap_search(url)
+    print(sitemap)
+    feeds = trafilatura.feeds.find_feed_urls(url)
     result = download_webpage(url)
+    print(result)
     
-    if result.get('error'):
-        print(f"Error: {result['error']}")
-    else:
-        print("Metadata:", result['metadata'])
-        print("\nExtracted Text:", result['text'][:500] if result['text'] else None)
+    #if result.get('error'):
+    #    print(f"Error: {result['error']}")
+    #else:
+    print("Metadata:", result['metadata'])
+    print("\nExtracted Text:", result['text'][:500] if result['text'] else None)
